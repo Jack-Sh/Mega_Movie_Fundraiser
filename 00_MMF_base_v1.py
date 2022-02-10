92c3620d16818e3b41208b816f8a4797a553588a
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-02-10 14:44:59 +1300

    v1 of name not blank function

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,29 @@
+# Import statements
+
+
+# Functions go here
+
+
+# ----- Main Routine -----
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they have used the program before & show instructions if answer is no
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method (apply surcharge if needed)
+
+# Calculate total sales and profit
+
+# Output data to text file
