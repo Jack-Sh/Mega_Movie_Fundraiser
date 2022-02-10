875b88ba4c71a8ea0e45d597253bce7e4aac182a
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-02-11 12:49:56 +1300

    added not_blank function to base

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -4,6 +4,22 @@
 # Functions go here
 
 
+# A function to check that the ticket name is not blank
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        # If the name is not blank, program continues
+        if response != "":
+            return response
+
+        # If the name is blank, show error and repeat the loop
+        else:
+            print("Sorry - this can't be blank, please enter your name")
+
+
 # ----- Main Routine -----
 
 # Set up dictionaries / lists needed to hold data
@@ -13,6 +29,7 @@
 # Loop to get ticket details
 
     # Get name (can't be blank)
+    name = not_blank("Name: ")
 
     # Get age (between 12 and 130)
 
