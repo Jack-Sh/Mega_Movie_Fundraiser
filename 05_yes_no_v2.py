c848f9b9bae61ec25a526c6a7cf0639f34acbb7b
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-02-16 09:49:30 +1300

    yes_no v1 complete, yes_no v2 complete (string checker)

---
+++ 05_yes_no_v2.py
@@ -0,0 +1,27 @@
+#string checker function, takes in question and list of valid responses
+def string_checker(question, to_check):
+
+    valid = False
+    while not valid:
+
+        error = "That is not a valid response"
+
+        # ask question and put response in lowercase
+        response = input(question).lower()
+
+        if response in to_check:
+            return response
+
+        else:
+            for item in to_check:
+                # checks if response is the first letter of an item in list
+                if response == item[0]:
+                    # returns entire response
+                    return item
+            else:
+                print(error)
+# Main routine
+for item in range(0, 6):
+    want_snacks = string_checker("Do you want snacks? ", ["yes", "no"])
+    print("Answer OK, you said:", want_snacks)
+    print()
