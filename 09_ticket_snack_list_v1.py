ca33d13797be61e3e98fe60824f4ae06a805afee
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-03-04 12:35:31 +1300

    ticket_snack_list complete

--- 09_ticket_snack_list_v1.py
+++ 09_ticket_snack_list_v1.py
@@ -15,7 +15,7 @@ snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 
 movie_data_dict = {
     'Name': all_names,
-    "Ticket": all_tickets,
+    ".Ticket.": all_tickets,
     "Popcorn": popcorn,
     "Water": water,
     "Pita Chips": pita_chips,
@@ -76,11 +76,15 @@ movie_frame = movie_frame.set_index("Name")
 # create column called 'Sub Total'
 # fill it price for snacks and ticket
 movie_frame["Sub Total"] = \
-    movie_frame["Ticket"] + \
+    movie_frame[".Ticket."] + \
     movie_frame["Popcorn"]*price_dict["Popcorn"] + \
     movie_frame["Water"]*price_dict["Water"] + \
     movie_frame["Pita Chips"]*price_dict["Pita Chips"] + \
     movie_frame["M&Ms"]*price_dict["M&Ms"] + \
     movie_frame["Orange Juice"]*price_dict["Orange Juice"]
+
+# shorten column names
+movie_frame = movie_frame.rename(columns ={'Orange Juice': 'OJ',
+                                            'Pita Chips': 'Chips'})
 
 print(movie_frame)
